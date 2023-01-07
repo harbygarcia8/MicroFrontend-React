14d1b87ff3b9d648367f7fcd87732f73a843d4e3
Harby Garcia <harby-96@hotmail.com>
2023-01-07 15:07:59 -0500

    Se hace us de useState, useEffects y lalóg cia delos hooks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState  } from 'react';
 import '@styles/header.scss';
+import Menu from '@components/Menu';
 
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
 
 function Header() {
+
+    const [toggle, setToggle] = useState (false);
+
+    const handleToggle = () => {
+        setToggle(!toggle);
+    }
+
     return (
         <nav>
             <img src={menu} alt="menu" className="menu" />
@@ -37,13 +45,14 @@ function Header() {
 
             <div className="navbar-right">
                 <ul>
-                    <li className="navbar-email">platzi@example.com</li>
+                    <li className="navbar-email" onClick=   {handleToggle}>harby-96@hotmail.com</li>
                     <li className="navbar-shopping-cart">
                         <img src={shoppingCart} alt="shopping cart" />
                         <div>2</div>
                     </li>
                 </ul>
             </div>
+            {toggle && <Menu />}            
         </nav>
     )
 }
